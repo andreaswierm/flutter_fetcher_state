7eca0bf0a55d59a25298d83c1fddffacf2283cdc
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2022-01-22 17:25:55 -0300

    add notfier callbacks to mutation

--- lib/flutter_fetcher_state.dart
+++ lib/flutter_fetcher_state.dart
@@ -4,3 +4,4 @@ export 'src/models/query/query.dart';
 export 'src/models/mutation/mutation.dart';
 export 'src/models/mutation_controller/mutation_controller.dart';
 export 'src/models/query_controller/query_controller.dart';
+export 'src/models/mutation_notifier/mutation_notifier.dart';
--- lib/src/models/mutation_controller/mutation_controller.dart
+++ lib/src/models/mutation_controller/mutation_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fetcher_state/src/models/mutation_notifier/mutation_notifier.dart';
 import 'package:flutter_fetcher_state/src/models/mutation_status_notifier/mutation_status_notifier.dart';
 
 typedef MutationCallback<T> = Future<T> Function(BuildContext);
@@ -8,7 +9,8 @@ class MutationController<T> extends MutationStatusNotifier<T> {
 
   MutationController({
     required this.context,
-  });
+    MutationNotifier<T>? notifier,
+  }) : super(notifier: notifier);
 
   void mutate(MutationCallback<T> mutation) async {
     setIsLoading();
